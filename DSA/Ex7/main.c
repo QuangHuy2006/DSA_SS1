e969e743d8445de525d666b2e4292017c4be3bf2
QuangHuy2006 <tonphamquanghuy3@Gmail.com>
2025-06-17 07:27:56 +0700

    Update main.c

--- DSA/Ex7/main.c
+++ DSA/Ex7/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int checking() {
-    int n[10]= {1,2,3,4,5,2,7,4,0,1};
+    int n[10]= {1,2,3,8,5,6,7,4,0,1};
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            if (n[i] == n[j]) {
+            if (n[i] == n[j] && j != i) {
                 return 1;
             }
         }
